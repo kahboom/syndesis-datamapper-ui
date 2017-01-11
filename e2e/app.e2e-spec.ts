6df983a2ccaf88882d62beb68ea2e4682c31e34d
Stan Lewis <slewis@fusesource.com>
2017-01-11 10:18:14 -0500

    Initial spike

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { DatamapperUiPage } from './app.po';
+
+describe('datamapper-ui App', function() {
+  let page: DatamapperUiPage;
+
+  beforeEach(() => {
+    page = new DatamapperUiPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
